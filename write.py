4ef2315a136204f0faa9d626f7b6fee7754a0e01
fishhuman <fish.human10@gmail.com>
2019-06-26 10:40:00 +0800

    add header

--- write.py
+++ write.py
@@ -1,4 +1,5 @@
 data = [1, 3, 5, 7, 9]
-with open('sequence.csv', 'w') as f:
+with open('sequence.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,價格\n')
     for d in data:
         f.write(str(d) + ',')
